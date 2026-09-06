d8b2ca30f3e36bff1485ada9556ae2561ec6f34d
Tiago Todescatto <texudev@yahoo.com>
2026-09-06 15:32:39 -0300

    Initial commit

---
+++ Medieval RPG.cpp
@@ -0,0 +1,12 @@
+﻿// Medieval RPG.cpp : Defines the entry point for the application.
+//
+
+#include "Medieval RPG.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
